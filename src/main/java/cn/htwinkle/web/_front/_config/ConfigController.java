44fa772f24b6d0cbc38b8be0214dc69aef09ec8d
TWINKLE <zhanghjqqqq@163.com>
2025-01-24 13:33:14 +0800

    配置中心支持swagger

--- src/main/java/cn/htwinkle/web/_front/_config/ConfigController.java
+++ src/main/java/cn/htwinkle/web/_front/_config/ConfigController.java
@@ -3,6 +3,9 @@ package cn.htwinkle.web._front._config;
 import cn.htwinkle.web.base.BaseController;
 import com.jfinal.aop.Inject;
 import com.jfinal.kit.Ret;
+import org.leon.swagger.annotation.Api;
+import org.leon.swagger.annotation.ApiOperation;
+import org.leon.swagger.model.constant.HttpMethod;
 
 
 /**
@@ -11,12 +14,16 @@ import com.jfinal.kit.Ret;
  * @author : twinkle
  * @date : 2020/3/9 21:46
  */
+@Api(tag = ConfigController.TAG, description = "配置中心")
 public class ConfigController extends BaseController {
+
+    protected static final String TAG = "CONFIG";
 
     @Inject
     private ConfigService configService;
 
     @Override
+    @ApiOperation(url = "/config/", tag = ConfigController.TAG, httpMethod = HttpMethod.GET, description = "获取配置信息")
     public void index() {
         renderJson(Ret.ok().set("list", configService.getList()));
     }
